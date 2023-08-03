b2999b589c51a4dfeac7afd2cb35e0b19cbdcb5e
Genone22 <100020872+Genone22@users.noreply.github.com>
2023-08-03 11:20:26 +0300

    Update parser.py
    match/case для Python версии 3.10+

--- parser.py
+++ parser.py
@@ -61,32 +61,22 @@ class ParseWB:
             _shot_id = product.id // 100000
             for product in item_model.products:
                 _short_id = product.id // 100000
-                if 0 <= _short_id <= 143:
-                    basket = '01'
-                elif 144 <= _short_id <= 287:
-                    basket = '02'
-                elif 288 <= _short_id <= 431:
-                    basket = '03'
-                elif 432 <= _short_id <= 719:
-                    basket = '04'
-                elif 720 <= _short_id <= 1007:
-                    basket = '05'
-                elif 1008 <= _short_id <= 1061:
-                    basket = '06'
-                elif 1062 <= _short_id <= 1115:
-                    basket = '07'
-                elif 1116 <= _short_id <= 1169:
-                    basket = '08'
-                elif 1170 <= _short_id <= 1313:
-                    basket = '09'
-                elif 1314 <= _short_id <= 1601:
-                    basket = '10'
-                elif 1602 <= _short_id <= 1655:
-                    basket = '11'
-                elif 1656 <= _short_id <= 1919:
-                    basket = '12'
-                else:
-                    basket = '13'
+                """Используем match/case для определения basket на основе _short_id"""
+                basket = match(_short_id,
+                    (range(0, 144), '01'),       # Если _short_id находится в диапазоне от 0 до 143, присвоить '01'
+                    (range(144, 288), '02'),     # И так далее для остальных диапазонов
+                    (range(288, 432), '03'),     
+                    (range(432, 720), '04'),
+                    (range(720, 1008), '05'),
+                    (range(1008, 1062), '06'),
+                    (range(1062, 1116), '07'),
+                    (range(1116, 1170), '08'),
+                    (range(1170, 1314), '09'),
+                    (range(1314, 1602), '10'),
+                    (range(1602, 1656), '11'),
+                    (range(1656, 1920), '12'),
+                    ('13')                      # Если _short_id не входит ни в один из предыдущих диапазонов, присвоить '13' basket-у
+                )
                 url = f"https://basket-{basket}.wb.ru/vol{_shot_id}/part{product.id // 1000}/{product.id}/images/big/1.jpg"
                 res = requests.get(url=url)
                 if res.status_code == 200:
